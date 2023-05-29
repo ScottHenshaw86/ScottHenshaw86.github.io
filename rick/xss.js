d11f2cdb7cad1349760beba8e01f29b28042b400
Scott Henshaw <scott.henshaw86@gmail.com>
2023-05-29 22:30:18 +0900

    comment xss

--- rick/xss.js
+++ rick/xss.js
@@ -1,11 +1,8 @@
-const btn = document.querySelector("button");
+// Here this is hardcoded to show what could happen.
+// But this kind of thing could be coming from a 3rd party. Don't blindly trust anyone!
+const data = `<iframe src="https://www.youtube.com/embed/xm3YgoEiEDc?rel=0&modestbranding=1&autohide=1&mute=1&showinfo=0&controls=0&autoplay=1"  width="560" height="315"  frameborder="0" allowfullscreen></iframe>`
+
 const h1 = document.querySelector("h1");
-
-
-const data = `<iframe src="https://www.youtube.com/embed/xm3YgoEiEDc?rel=0&modestbranding=1&autohide=1&mute=1&showinfo=0&controls=0&autoplay=1"  width="560" height="315"  frameborder="0" allowfullscreen></iframe>
-`
-console.log("something");
-
 const getDataAndShow = () => {
     h1.innerHTML = data;
     console.log("something");
